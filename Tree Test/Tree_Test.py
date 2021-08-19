d1b337bbed7f106080886cd3aa8ff291b96c1165
Spenser Short <spenscershort@gmail.com>
2021-08-19 13:14:34 -0400

    Worked on removal, insertion, and printing. None of which seem to be working. Will complete tomorrow

--- Tree Test/Tree_Test.py
+++ Tree Test/Tree_Test.py
@@ -1,7 +1,7 @@
 # This program is to test trees in python. For now, I'm only using a binary tree
 
 class TreeNode: # normal child node. For the root, None will be passed
-    value = None
+    value = 0
     leftChild = None
     rightChild = None
     parent = None
@@ -17,41 +17,84 @@ def printTraverse(node): # recursive algorithm to traverse tree and print
         printTraverse(node.rightChild) # move further right
     return # go to parent
 
-def insertVal(val): # TODO: Complete insertion 
-    pass
+def insertVal(val, root): # TODO: Complete insertion
+    val = int(val)
+    root.value = int(root.value)
+    if val <= root.value: # sort down
+        if root.leftChild != None: # if not at end of branch, recur
+            insertVal(val, root.leftChild)
+        else: # if at end of branch, add new node
+            newNode = TreeNode
+            newNode.parent = root
+            newNode.value = val
+            root.leftChild = newNode
+            newNode.leftChild = None
+            newNode.rightChild = None
+    else: # sort up
+        if root.rightChild != None: # if not at end of branch, recur
+            insertVal(val, root.rightChild)
+        else: # if at end of branch, add new node
+            newNode = TreeNode(val, root)
+            root.rightChild = newNode
+            newNode.leftChild = None
+            newNode.rightChild = None
 
-def insertNode(node, root): # TODO: finish removal process
-    if node.value < root.value:
-        pass
-    elif node.value > root.value:
-        pass
 
+def removeNode(root, value): # recursive algorithm to remove a value
+    node = treeSearch(value, root) # value does not exist
+    if node == None:
+        print("No such value in tree")
+        return
+    parent = node.parent
+    left = False
+    if parent.leftChild == node:
+        left = True
 
-def removeTraverse(node, value, root): # recursive algorithm to remove a value
-    if node.value == value: # checking node value
-        parent = node.parent
-        if node.leftChild != None:
-            if parent.leftChild == node:
-                parent.leftChild = node.leftChild # incomplete: only takes care of one child, cannot do both
-            else:
-                parent.rightChild = node.leftChild
-        elif node.rightChild != None:
-            if parent.leftChild == node:
-                parent.leftChild = node.rightChild
-            else:
-                parent.rightChild = node.rightChild
+    if node.leftChild == None: # node has 1 or 0 children, simple replacement.
+        if left:
+            parent.leftChlid = node.rightChild # replace child
+            parent.leftChild.parent = parent # replace parent
         else:
-            if parent.leftChild == node:
-                parent.leftChild = None
-            else:
-               parent.rightChild = None
+            parent.rightChild = node.rightChild # replace child
+            parent.rightChild.parent = parent # replace parent
+        return
+    elif node.rightChild == None:
+        if left:
+            parent.leftChild = node.leftChild # replace child
+            parent.leftChild.parent = parent # replace parent
+        else:
+            parent.rightChild = node.leftChild # replace child
+            parent.rightChild.parent = parent # replace parent
+        return
+    # how to do this sourced from here: https://www.geeksforgeeks.org/binary-search-tree-set-2-delete/
+    # executes if node has 2 children
+    replacement = findMaxVal(node.leftChild) # find replacment value
+    node.value = replacement.value # replace value only
+    removeNode(replacement, replacement.value) # recur in order to get rid of replacement 
 
-    if node.leftChild != None:
-        printTraverse(node.leftChild)
 
-    if node.rightChild != None:
-        printTraverse(node.rightChild)
-    return
+
+def findMaxVal(root):
+    if root.rightChild == None:
+        return root
+    else:
+        return findMaxVal(root.rightChild)
+
+
+
+def treeSearch(value, node):
+    current = node
+    if current == None: # if we reach end of tree
+        return None
+    if value != current.value: # when we are not in the correct node
+        if value <= current.value:
+            current = current.leftChild # get closer
+            treeSearch(value, current) # recur
+        elif value > current.value:
+            current = current.rightChild # get closer
+            treeSearch(value, current)
+    else: # if we are in the correct node
+        return current
 
 root = None
 current = None
@@ -60,21 +103,31 @@ while True:
     print("1: add value\n"+ # menu stolen from my previous projects
         "2: remove value\n"+
         "3: print tree\n"+
-        "4: create new tree"+
+        "4: create new tree\n"+
         "5: quit\n")
     choice = input("enter choice: ")
     if choice == "4":
         val = input("Enter the root value: ")
         root = TreeNode(val, None)
-    if choice == "3":
+    elif choice == "3":
         if root == None:
             print("No tree to print")
         else:
             printTraverse(root)
-    if choice == "2":
+    elif choice == "2":
         if root == None:
             print("No tree to remove from")
         else:
-            val = input("Enter value to be removed: ")
+            val = int(input("Enter value to be removed: "))
+            removeNode(root, val)
+    elif choice == "1":
+        if root != None:
+            val = int(input("Enter new value: "))
+            insertVal(val, root)
+        else:
+            print("No tree to add to")
+    elif choice == "5":
+        break
+
 
             
