c8b8346e4382d439a4aa5129edf17ac6b6a3f819
Spenser Short <spenscershort@gmail.com>
2021-08-24 12:54:27 -0400

    Finished test, trees working as intended

--- Tree Test/Tree_Test.py
+++ Tree Test/Tree_Test.py
@@ -10,28 +10,26 @@ class TreeNode: # normal child node. For the root, None will be passed
         self.parent = parent
 
 def printTraverse(node): # recursive algorithm to traverse tree and print
-    if node.leftChild != None: # check if we are all the way left
+    if node.leftChild is not None: # check if we are all the way left
         printTraverse(node.leftChild) # move further left
     print(node.value) # print the value of the node
-    if node.rightChild != None: # now check to make sure we are all the way right
+    if node.rightChild is not None: # now check to make sure we are all the way right
         printTraverse(node.rightChild) # move further right
     return # go to parent
 
-def insertVal(val, root): # TODO: Complete insertion
+def insertVal(val, root):
     val = int(val)
     root.value = int(root.value)
     if val <= root.value: # sort down
-        if root.leftChild != None: # if not at end of branch, recur
+        if root.leftChild is not None: # if not at end of branch, recur
             insertVal(val, root.leftChild)
         else: # if at end of branch, add new node
-            newNode = TreeNode
-            newNode.parent = root
-            newNode.value = val
+            newNode = TreeNode(val, root)
             root.leftChild = newNode
             newNode.leftChild = None
             newNode.rightChild = None
     else: # sort up
-        if root.rightChild != None: # if not at end of branch, recur
+        if root.rightChild is not None: # if not at end of branch, recur
             insertVal(val, root.rightChild)
         else: # if at end of branch, add new node
             newNode = TreeNode(val, root)
@@ -42,15 +40,23 @@ def insertVal(val, root): # TODO: Complete insertion
 
 def removeNode(root, value): # recursive algorithm to remove a value
     node = treeSearch(value, root) # value does not exist
-    if node == None:
+    if node is None:
         print("No such value in tree")
         return
     parent = node.parent
-    left = False
-    if parent.leftChild == node:
+    if parent is None: # check if removal is root. if yes, change course
+        removeRoot(node)
+        return
+    left = False # check which child removal is
+    if parent.leftChild is node:
         left = True
-
-    if node.leftChild == None: # node has 1 or 0 children, simple replacement.
+    if node.leftChild is None and node.rightChild is None: # node has 1 or 0 children, simple replacement/deletion.
+        if left:
+            parent.leftChild = None # simple deletion on left
+        else:
+            parent.rightChild = None #simple deletion on right
+        return
+    elif node.leftChild is None: 
         if left:
             parent.leftChlid = node.rightChild # replace child
             parent.leftChild.parent = parent # replace parent
@@ -58,7 +64,7 @@ def removeNode(root, value): # recursive algorithm to remove a value
             parent.rightChild = node.rightChild # replace child
             parent.rightChild.parent = parent # replace parent
         return
-    elif node.rightChild == None:
+    elif node.rightChild is None:
         if left:
             parent.leftChild = node.leftChild # replace child
             parent.leftChild.parent = parent # replace parent
@@ -73,26 +79,35 @@ def removeNode(root, value): # recursive algorithm to remove a value
     removeNode(replacement, replacement.value) # recur in order to get rid of replacement 
 
 
+def removeRoot(root): # if value to removed is the root with no parents, the algorithm must be adjusted
+    if root.leftChild is None and root.rightChild is None:
+        print("Cannot delete only value in tree: Please create new tree")
+    if root.leftChild is not None:
+        replacement = findMaxVal(root.leftChild) # find replacment value
+    else:
+        replacement = findMaxVal(root.rightChild)
+    root.value = replacement.value # replace value only
+    removeNode(replacement, replacement.value) # recur in order to get rid of replacement 
 
 def findMaxVal(root):
-    if root.rightChild == None:
+    if root.rightChild is None:
         return root
     else:
         return findMaxVal(root.rightChild)
 
 
 
-def treeSearch(value, node):
+def treeSearch(value, node): # this function is used to find the replacement value
     current = node
-    if current == None: # if we reach end of tree
+    if current is None: # if we reach end of tree
         return None
-    if value != current.value: # when we are not in the correct node
+    if value is not current.value: # when we are not in the correct node
         if value <= current.value:
             current = current.leftChild # get closer
-            treeSearch(value, current) # recur
+            return treeSearch(value, current) # recur
         elif value > current.value:
             current = current.rightChild # get closer
-            treeSearch(value, current)
+            return treeSearch(value, current)
     else: # if we are in the correct node
         return current
 
@@ -128,6 +143,3 @@ while True:
             print("No tree to add to")
     elif choice == "5":
         break
-
-
-            
